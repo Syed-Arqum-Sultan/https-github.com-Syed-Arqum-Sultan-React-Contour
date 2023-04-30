5575e64451af4cd72f5c7d9d94f5829d053ddc27
Syed-Arqum-Sultan <110258159+Syed-Arqum-Sultan@users.noreply.github.com>
2023-05-01 00:44:27 +0500

    Initial React Lifecycle using react useEffect

--- App.tsx
+++ App.tsx
@@ -1,11 +1,49 @@
 import * as React from 'react';
 import './style.css';
 
-export default function App() {
+export default function Counter() {
+  const [show, setShow] = React.useState(false)
+
+
+//   const handleToggle = () => {
+//     setShow(!show)
+//   }
+// // 
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <button onClick={(e)=> {
+        e.preventDefault;
+        setShow(!show);
+      }}>{show ? 'hide' : 'show'} counter</button>
+    {show ? <DisplayCounter /> : null}
     </div>
-  );
+  )
+  
+}
+
+function DisplayCounter() {
+  const [state, setCount] = React.useState({ count: 0, name: 'John' });
+
+  React.useEffect(()=>{
+    console.log('mount');
+
+    return () => {
+      console.log('unmount')
+    }
+  }, [])
+
+  React.useEffect(() => {
+    console.log(state)
+  }, [state.count])
+  
+  const handleClick = () => {
+    setCount({ ...state, count: state.count + 1})
+   }
+
+   return (
+    <div>
+      <p>{state.count}</p>
+      <button onClick={handleClick}>Increment</button>
+      </div>
+  )
 }
